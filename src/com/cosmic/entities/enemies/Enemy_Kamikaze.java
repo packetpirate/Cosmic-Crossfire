f931d47b4e0dae8cb2378655e7afa4af64ff5cb3
Darin Beaudreau <darinbeaudreau@gmail.com>
2016-09-10 14:59:35 -0400

    Fixes to kamikaze ship class.

--- src/com/cosmic/entities/enemies/Enemy_Kamikaze.java
+++ src/com/cosmic/entities/enemies/Enemy_Kamikaze.java
@@ -1,8 +1,12 @@
 package com.cosmic.entities.enemies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.cosmic.behaviors.MovementBehavior;
 import com.cosmic.behaviors.WeaponBehavior;
 import com.cosmic.entities.Enemy;
+import com.cosmic.entities.Projectile;
 import com.cosmic.utils.Pair;
 
 import javafx.scene.canvas.GraphicsContext;
@@ -11,18 +15,17 @@ public class Enemy_Kamikaze extends Enemy{
 
 	public Enemy_Kamikaze(Pair<Double> pos) {
 		super(pos, MovementBehavior.SHIP_KAMIK, WeaponBehavior.BASIC_FIRE);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void update(long currentTime, Pair<Double> playerPos) {
-		// TODO Auto-generated method stub
+	public List<Projectile> update(long currentTime, Pair<Double> playerPos) {
+		List<Projectile> shots = new ArrayList<Projectile>();
 		
+		return shots;
 	}
 
 	@Override
 	public void render(GraphicsContext gc) {
-		// TODO Auto-generated method stub
 		
 	}
 
