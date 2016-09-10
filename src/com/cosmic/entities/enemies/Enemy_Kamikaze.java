fced3c863905e0fce6e3757e2c6a0826085591c2
timLarocque <timothy_larocque@student.uml.edu>
2016-09-10 14:29:02 -0400

    Base Enemy_Kamikaze Class created

---
+++ src/com/cosmic/entities/enemies/Enemy_Kamikaze.java
@@ -0,0 +1,29 @@
+package com.cosmic.entities.enemies;
+
+import com.cosmic.behaviors.MovementBehavior;
+import com.cosmic.behaviors.WeaponBehavior;
+import com.cosmic.entities.Enemy;
+import com.cosmic.utils.Pair;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class Enemy_Kamikaze extends Enemy{
+
+	public Enemy_Kamikaze(Pair<Double> pos, MovementBehavior mb, WeaponBehavior wb) {
+		super(pos, mb, wb);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(long currentTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
